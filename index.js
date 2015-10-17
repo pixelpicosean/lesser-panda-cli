ad2bb50500c566386ca24082b51a8afba2fac718
Sean Bohan <pixelpicosean@gmail.com>
2015-10-17 09:16:48 +0800

    Start with simple webpack based live-reload server.

---
+++ index.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+var path = require('path');
+
+var webpack = require('webpack');
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var WebpackDevServer = require('webpack-dev-server');
+
+// TODO: find an available port
+var port = 4000;
+var gameDir = process.cwd();
+
+var config = {
+  devtool: 'eval',
+  entry: {
+    app: [
+      path.resolve(gameDir, 'game/main.js')
+    ]
+  },
+  output: {
+    path: path.resolve(gameDir, 'dist'),
+    publicPath: 'http://localhost:4000/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      reloadScript: 'http://localhost:' + port + '/webpack-dev-server.js',
+      template: path.resolve(gameDir, 'index.html'),
+      inject: 'body',
+      filename: 'index.html'
+    }),
+  ],
+};
+
+var compiler = webpack(config);
+
+var server = new WebpackDevServer(compiler, {
+  hot: false,
+
+  quiet: false,
+  noInfo: false,
+  lazy: false,
+
+  stats: {
+    colors: true,
+  },
+});
+server.listen(port, "localhost", function() {});
+// server.close();
