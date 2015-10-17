47f16fa23632fa69de7dd235825855c679cf4896
Sean Bohan <pixelpicosean@gmail.com>
2015-10-17 21:21:59 +0800

    Use strict mode for scripts.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var commands = {
   build: 'Build project to "dist" folder',
   create: 'Create new project',
---
+++ lint.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = function() {
+  console.log('Lint not work yet.');
+};
