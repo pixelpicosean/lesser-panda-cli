62cfa36680c78a6e6fbc283824e4b056803e6eb9
Sean Bohan <pixelpicosean@gmail.com>
2015-11-16 18:13:53 +0800

    Reset devtool to eval-source-map for performance. Add reload script as entry to avoid script injection. Make it possible to load from other devices within the same local network.

--- server.js
+++ server.js
@@ -6,14 +6,32 @@ var webpack = require('webpack');
 var HTMLWebpackPlugin = require('html-webpack-plugin');
 var WebpackDevServer = require('webpack-dev-server');
 
+function getIPAddress() {
+  var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+        return alias.address;
+    }
+  }
+  return '0.0.0.0';
+}
+
 // TODO: find an available port or manually set one
 var port = 4000;
+var fullAddress = getIPAddress() + ':' + port;
 
 function server(gameDir) {
   var config = {
-    devtool: 'source-map',
+    devtool: 'eval-source-map',
     entry: {
-      app: path.resolve(gameDir, 'src/game/main.js'),
+      app: [
+        'webpack-dev-server/client?http://' + fullAddress,
+        path.resolve(gameDir, 'src/game/main.js')
+      ],
     },
     output: {
       path: path.resolve(gameDir, 'dist'),
@@ -24,7 +42,6 @@ function server(gameDir) {
     },
     plugins: [
       new HTMLWebpackPlugin({
-        reloadScript: 'http://localhost:' + port + '/webpack-dev-server.js',
         template: path.resolve(gameDir, 'index.html'),
         inject: 'body',
         filename: 'index.html'
@@ -49,6 +66,7 @@ function server(gameDir) {
     },
     resolve: {
       root: path.join(gameDir, 'src'),
+      fallback: path.join(__dirname, 'node_modules'),
     },
     resolveLoader: {
       root: path.join(__dirname, 'node_modules'),
@@ -68,11 +86,11 @@ function server(gameDir) {
       colors: true,
     },
   });
-  devServer.listen(port, "localhost", function() {
+  devServer.listen(port, null, function() {
     // TODO: purple "LP" and red url
     console.log('[LP] Server started:');
     console.log(' --------------------------');
-    console.log(' URL: http://localhost:4000');
+    console.log(' URL: http://' + fullAddress);
     console.log(' --------------------------\n');
   });
 }
