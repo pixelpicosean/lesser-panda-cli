28b78b2f5544353da017b66803d2f57de972ea6c
Sean Bohan <pixelpicosean@gmail.com>
2016-11-05 08:24:37 +0800

    Make ES6 to ES5 transpile configable with “-es5”.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var commands = {
-  build: 'Build project to "dist" folder, with "-u" to disable uglify',
+  build: 'Build project to "dist" folder, with "-u" to disable uglify, and "-es5" to transpile for ES5',
   create: 'Create new project',
   // update: 'Update engine',
-  server: 'Start a dev server',
+  server: 'Start a dev server, with "-es5" to enable transpile to ES5, otherwise you need a ES2015 capatable browser',
 };
 
 var lpanda = {
