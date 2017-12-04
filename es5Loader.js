b62b1ba0b111fa75e56edfe0adeef305eb71c759
Sean Bohan <pixelpicosean@gmail.com>
2017-12-04 16:06:13 +0800

    Use babel-preset-env instead of babel-preset-es2015

--- es5Loader.js
+++ es5Loader.js
@@ -7,7 +7,6 @@ module.exports = function(gameDir) {
     test: /\.js$/,
     include: path.resolve(gameDir, 'src'),
     exclude: [
-      path.resolve(gameDir, 'src/engine/audio/hower.core.js'),
       path.resolve(gameDir, 'src/engine/polyfill'),
     ],
     use: [
@@ -15,7 +14,7 @@ module.exports = function(gameDir) {
         loader: require.resolve('babel-loader'),
         options: {
           presets: [
-            [require.resolve('babel-preset-es2015'), { loose: true }],
+            [require.resolve('babel-preset-env'), { loose: true }],
           ],
           plugins: [
             [require.resolve('babel-plugin-transform-strict-mode'), { strict: true }],
