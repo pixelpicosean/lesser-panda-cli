8aab0037571d123a487f4aa4cbf3b0de83756c27
Sean Bohan <pixelpicosean@gmail.com>
2020-08-28 08:02:26 +0800

    Use 0.0.0.0 host address

--- server.js
+++ server.js
@@ -22,16 +22,6 @@ const services = [
 ]
 
 function getIPAddress() {
-  const interfaces = require('os').networkInterfaces();
-  for (let devName in interfaces) {
-    let iface = interfaces[devName];
-
-    for (let i = 0; i < iface.length; i++) {
-      let alias = iface[i];
-      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
-        return alias.address;
-    }
-  }
   return '0.0.0.0';
 }
 
